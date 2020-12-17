a3e9a67b01f74afed17736623814a5c0e613a20f
RC <51386657+UnfortunateComplexity@users.noreply.github.com>
2020-12-17 09:13:10 -0700

    Added some changes including deleteUser() method

--- auth3/api/objects/user.php
+++ auth3/api/objects/user.php
@@ -20,15 +20,12 @@ class User{
 // create new user record
 function create(){ 
     // insert query
-     $query = "INSERT INTO " . $this->table_name . "
-            SET
-                username = :username,
-                usertype = :usertype,
-                password = :password";
-    // prepare the query
-    $stmt = $this->conn->prepare($query);
+     $query_create = "INSERT INTO " . $this->table_name . "
+            SET  username = :username, usertype = :usertype, password = :password";
+    // prepare the insert query
+    $stmt = $this->conn->prepare($query_create);
  
-    // sanitize
+    // Clean up the inputs
     $this->username=htmlspecialchars(strip_tags($this->username));
     $this->usertype=htmlspecialchars(strip_tags($this->usertype));
     $this->password=htmlspecialchars(strip_tags($this->password));
@@ -43,15 +40,15 @@ function create(){
  
     // execute the query, also check if query was successful
     if($stmt->execute()){ return true; }
+ 
     //else query execution failed
     return false;
 }
  
-// emailExists() method will be here
-// check if given email exist in the database
+// check if given username exists in the database
 function usernameExists(){
  
-    // query to check if email exists
+    // query to check if username exists
     $query = "SELECT *
             FROM " . $this->table_name . "
             WHERE username = ?
@@ -60,10 +57,10 @@ function usernameExists(){
     // prepare the query
     $stmt = $this->conn->prepare( $query );
  
-    // sanitize
+    // clean the input username
     $this->username=htmlspecialchars(strip_tags($this->username));
  
-    // bind given email value
+    // bind it
     $stmt->bindParam(1, $this->username);
  
     // execute the query
@@ -72,22 +69,22 @@ function usernameExists(){
     // get number of rows
     $num = $stmt->rowCount();
  
-    // if email exists, assign values to object properties for easy access and use for php sessions
+    // if username exists, assign values to object properties for easy access and use for php sessions
     if($num>0){
- 
         // get record details / values
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
  
         // assign values to object properties
         $this->id = $row['id'];
         $this->username = $row['username'];
+        $this->usertype = $row['usertype'];
         $this->password = $row['password'];
  
-        // return true because email exists in the database
+        // return true because the username exists in the database
         return true;
     }
  
-    // return false if email does not exist in the database
+    // false --> username is not in database.
     return false;
 }
  
@@ -110,7 +107,7 @@ public function update(){
     $stmt = $this->conn->prepare($query);
  
     // sanitize
-    $this->uusername=htmlspecialchars(strip_tags($this->username));
+    $this->username=htmlspecialchars(strip_tags($this->username));
     $this->usertype=htmlspecialchars(strip_tags($this->usertype));
  
     // bind the values from the form
@@ -134,4 +131,27 @@ public function update(){
  
     return false;
 }
+
+function deleteUser(){
+     // delete query
+    $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+  
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+  
+    // sanitize
+    $this->id=htmlspecialchars(strip_tags($this->id));
+  
+    // bind id of record to delete
+    $stmt->bindParam(1, $this->id);
+  
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+  
+    return false;
+
+}
+
 }
