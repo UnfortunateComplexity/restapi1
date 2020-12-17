348e8ec823b1050c989868fbfcec052c54c3ee7a
RC <51386657+UnfortunateComplexity@users.noreply.github.com>
2020-12-17 08:47:42 -0700

    Changes

--- auth3/api/login.php
+++ auth3/api/login.php
@@ -1,12 +1,11 @@
 <?php
 // required headers
-header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Access-Control-Allow-Origin: http://localhost/auth3/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
-// database connection will be here
 // files needed to connect to database
 include_once 'config/database.php';
 include_once 'objects/user.php';
@@ -18,15 +17,13 @@ $db = $database->getConnection();
 // instantiate user object
 $user = new User($db);
  
-// check email existence here
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
  
 // set product property values
 $user->username = $data->username;
-$username_exists = $user->usernameExists();
+$username_exists = $user->usernameExists();					//To check if username is already registered in the database.
  
-// files for jwt will be here
 // generate json web token
 include_once 'config/core.php';
 include_once 'libs/php-jwt-master/src/BeforeValidException.php';
@@ -35,8 +32,7 @@ include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
 include_once 'libs/php-jwt-master/src/JWT.php';
 use \Firebase\JWT\JWT;
  
-// generate jwt will be here
-// check if email exists and if password is correct
+// check if username is registered in the database and if password is correct
 if($username_exists && password_verify($data->password, $user->password)){
 	$token = array(
 		"iat" => $issued_at,
@@ -57,13 +53,12 @@ if($username_exists && password_verify($data->password, $user->password)){
     echo json_encode(array("message" => "Successful login.","jwt" => $jwt));
 }
  
-// login failed will be here
 // login failed
 else{
-// set response code
+	// set response code
 	http_response_code(401);
  
     // tell the user login failed
-    echo json_encode(array("message" => "Login failed."));
+    echo json_encode(array("message" => "Login failed! Username or password is incorrect."));
 }
 ?>
